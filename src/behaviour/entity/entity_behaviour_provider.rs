e7500f36453472eae59bd3ade3c8589a0d73e220
Andreas Schaeffer <andreas@schaeffernet.de>
2022-02-05 22:52:04 +0100

    Refactor di

--- src/behaviour/entity/entity_behaviour_provider.rs
+++ src/behaviour/entity/entity_behaviour_provider.rs
@@ -1,9 +1,9 @@
 use std::sync::Arc;
 
+use crate::di::*;
 use async_trait::async_trait;
 use log::debug;
 use uuid::Uuid;
-use waiter_di::*;
 
 use crate::behaviour::entity::system_command::SystemCommand;
 use crate::model::ReactiveEntityInstance;
@@ -14,7 +14,7 @@ const SYSTEM_COMMAND: &'static str = "system_command";
 #[wrapper]
 pub struct SystemCommandStorage(std::sync::RwLock<std::collections::HashMap<Uuid, std::sync::Arc<SystemCommand>>>);
 
-#[waiter_di::provides]
+#[provides]
 fn create_system_command_storage() -> SystemCommandStorage {
     SystemCommandStorage(std::sync::RwLock::new(std::collections::HashMap::new()))
 }
